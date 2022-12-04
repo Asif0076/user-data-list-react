0b2f3581da258822c6ffb731b1763f53c65302e3
Asif Nawaz <assu.nawaz007@gmail.com>
2022-12-05 03:44:12 +0500

    added input and add addinput

--- src/App.js
+++ src/App.js
@@ -2,33 +2,56 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [inputDataList, setInputDataList ] = useState([]);
+const [inputData, setInputData] = useState(
+  {
+    name: " ",
+    age: " ",
+    designation: " "
+}
+);
 
-const [inputData, setInputData] = useState("");
 
+function handleChange(e) {
+console.log(e.target.name,e.target.value);
+setInputData({...inputData, [e.target.name]: e.target.value});
+}
 
-function handleChange() {
-
+function addData() {
+setInputDataList([...inputDataList, inputData ])
+console.log([...inputDataList, inputData ]);
 }
 
 
   return (
-    <>
+    <>  
+
         <h1>User Data</h1>
+        <h2>Enter Your Data </h2>
         <div className='inputs'>
           <div className='head-input' >
-           <h2>Name</h2>
-            <input type="text" name='name' placeholder="Enter Your Name" onChange={handleChange} value={inputData} />
+           <h3>Name</h3>
+            <input type="text" name='name' placeholder="Enter Your Name" onChange={handleChange} value={inputData.name} />
         </div>
         <div className='head-input'>
-           <h2>Name</h2>
-            <input type="text" name='name' placeholder="Enter Your Name" onChange={handleChange} value={inputData} />
+           <h3>Age</h3>
+            <input type="text" name='age' placeholder="Enter Your Name" onChange={handleChange} value={inputData.age} />
         </div>
         <div className='head-input'>
-           <h2>Name</h2>
-            <input type="text" name='name' placeholder="Enter Your Name" onChange={handleChange} value={inputData} />
+           <h3>Designation</h3>
+            <input type="text" name='designation' placeholder="Enter Your Name" onChange={handleChange} value={inputData.designation} />
         </div> 
         </div>
-
+             <button className='btn-add' onClick={addData}>Add</button>
+       
+        <table>
+          <tr>
+            <td>Name</td>
+            <td>Age</td>
+            <td>designation</td>
+            <td>Remove</td>
+          </tr>
+        </table>
     </>
   );
 }
