246899dcac1991fcc3a1d52d88e86cca39bff97a
Asif Nawaz <assu.nawaz007@gmail.com>
2022-12-05 01:40:33 +0500

    added new image and css to file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [inputData, setInputData] = useState("");
+
+
+function handleChange() {
+
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <h1>User Data</h1>
+        <div className='inputs'>
+          <div className='head-input' >
+           <h2>Name</h2>
+            <input type="text" name='name' placeholder="Enter Your Name" onChange={handleChange} value={inputData} />
+        </div>
+        <div className='head-input'>
+           <h2>Name</h2>
+            <input type="text" name='name' placeholder="Enter Your Name" onChange={handleChange} value={inputData} />
+        </div>
+        <div className='head-input'>
+           <h2>Name</h2>
+            <input type="text" name='name' placeholder="Enter Your Name" onChange={handleChange} value={inputData} />
+        </div> 
+        </div>
+
+    </>
   );
 }
 
